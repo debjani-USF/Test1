5963a6eeea8aee36a990839d6095df85d583d987
debjani <debjani2907@gmail.com>
2025-02-26 10:33:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Debug.WriteLine("Hello, World!");
+        Debug.WriteLine("hello");
+
+
+        int x = 3;
+        int Y = 10;
+    }
+}
